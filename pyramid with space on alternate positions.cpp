268d26408d73965f0859595a1e064349cda0b21e
Senthil Lakshmikanth <lakshmikanthsenthil@gmail.com>
2025-01-02 22:42:58 +0530

    Update pyramid with space on alternate positions.cpp

--- pyramid with space on alternate positions.cpp
+++ pyramid with space on alternate positions.cpp
@@ -63,3 +63,48 @@ int main()
 
     return 0;
 }
+
+// Online C++ compiler to run C++ program online
+* * * * *
+ * * * * 
+  * * *  
+   * *   
+    *    
+    *    
+   * *   
+  * * *  
+ * * * * 
+* * * * *
+   
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    for(int i=1; i<=10; i++)
+    {
+        for(int j=1; j<10; j++)
+        {
+            if(i<=5)
+                if(j>=i and j<=10-i)
+                    if((i%2==1 and j%2==1) or (i%2==0 and j%2==0))
+                        cout<<"*";
+                    else
+                        cout<<" ";
+                else
+                    cout<<" ";
+            else
+                if(j>10-i and j<i)
+                    if((i%2==1 and j%2==1) or (i%2==0 and j%2==0))
+                        cout<<" ";
+                    else
+                        cout<<"*";
+                else
+                    cout<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
